660a7a40a7e316762813a428022eb09569035097
veekshashetty11 <veekshavshetty.cs24@rvce.edu.in>
2026-01-16 11:35:08 +0530

    Add initial C++ program structure

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Automated Exam Allocation System" << endl;
+    cout << "DSA-based multi-constraint scheduling project" << endl;
+    return 0;
+}
